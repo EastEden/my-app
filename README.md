ef0e7591410be74b5d44dc86ce035f9bc3b9c028
EastEden <alexis10cap@gmail.com>
2021-12-04 14:05:18 +0800

    Some Updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 This is my Sample Apps using React Frame as a self-learning procedure.
+Node.js is used as my backend.
+
+#JSX
+Introduces the basics of JSX Components.
+{{}} should be used to refer JS Code.
+HTML Renaming is clear the _ and replace by Capital Letter.
+
+#Components
+Introduces Child Component.
+
+#Seasons
+Some functions.
--- components/src/index.js
+++ components/src/index.js
@@ -5,9 +5,15 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Yestarday" text="Nice"/>
-            <CommentDetail author="Alex" timeAgo="Jan.1st" text="OK"/>
-            <CommentDetail author="Bob" timeAgo="Today" text="Fantastic"/>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Yestarday" text="Nice"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Jan.1st" text="OK"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Bob" timeAgo="Today" text="Fantastic"/>
+            </ApprovalCard>
         </div>
     );
 };
